3925449dfb85a1cd90a380220284a1a1e1933cd3
Shafiya-Heena <shafiya@creativecommons.org>
2025-10-20 16:02:14 +0000

    updated startup and docker compose

--- config/startupservice.sh
+++ config/startupservice.sh
@@ -15,11 +15,11 @@ echo "${E1}Waiting for database at ${DB_HOST}:${DB_PORT}...${E0}"
 
 # Proper syntax for /dev/tcp — NOTE: no colon between host and port
 while ! (echo > /dev/tcp/${DB_HOST}/${DB_PORT}) >/dev/null 2>&1; do
-  echo "Database not reachable yet. Retrying in 2s..."
-  sleep 2
+  echo "Database not reachable yet. Retrying in 1s..."
+  sleep 1
 done
 
-echo "${E1}Database is up! Starting webserver at http://127.0.0.1:8080${E0}"
+echo "${E1}Database is up! Starting apache2 webserver ${E0}"
 
 # Start Apache in the foreground
 /sbin/apache2ctl -D FOREGROUND -k start
