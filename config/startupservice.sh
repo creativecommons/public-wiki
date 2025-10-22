c8a6bfad0a2e440871050bd573f9d0f298ac329f
Shafiya-Heena <shafiya@creativecommons.org>
2025-10-22 16:04:49 +0000

    updated the database messages not to be bold and spilt the apache2 server message

--- config/startupservice.sh
+++ config/startupservice.sh
@@ -11,15 +11,16 @@ E1="$(printf "\e[1m")"        # bold
 DB_HOST=${WIKI_DB_HOST:-wiki-db}
 DB_PORT=${WIKI_DB_PORT:-3306}
 
-echo "${E1}Waiting for database at ${DB_HOST}:${DB_PORT}...${E0}"
+echo "$Waiting for database at ${DB_HOST}:${DB_PORT}..."
 
-# Proper syntax for /dev/tcp — NOTE: no colon between host and port
+# Note proper syntax for /dev/tcp — no colon between host and port
 while ! (echo > /dev/tcp/${DB_HOST}/${DB_PORT}) >/dev/null 2>&1; do
   echo "Database not reachable yet. Retrying in 1s..."
   sleep 1
 done
 
-echo "${E1}Database is up! Starting apache2 webserver ${E0}"
+echo 'Database is up!'
 
 # Start Apache in the foreground
+echo "${E1}Starting apache2 webserver: http://localhost:8080/${E0}"
 /sbin/apache2ctl -D FOREGROUND -k start
