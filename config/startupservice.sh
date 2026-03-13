f8cf41b483027d3b25cc7a567a29e24a7e9ce4df
Timid Robot Zehta <timid@creativecommons.org>
2026-03-13 23:32:05 +0100

    fix startup variable test and improve apache2 config
    - use more common MYSQL_ variable names instead of MARIADB_
    - fix MediaWiki short URLs per documentation:
      https://www.mediawiki.org/wiki/Manual:Short_URL/Apache
    - config/startupservice.sh
      - fix variable check. also see:
        https://stackoverflow.com/questions/16553089/dynamic-variable-names-in-bash
      - use 4 space hard tabs
      - make output more colorful
    - config/web-sites-available/000-default.conf
      - use directory config instead of .htaccess (more secure, better
        performance)

--- config/startupservice.sh
+++ config/startupservice.sh
@@ -4,39 +4,44 @@ set -o nounset
 
 # https://en.wikipedia.org/wiki/ANSI_escape_code
 E0="$(printf "\e[0m")"        # reset
-E1="$(printf "\e[1m")"        # bold
+E31="$(printf "\e[31m")"      # foreground: red
+E90="$(printf "\e[90m")"      # foreground: bright black (gray)
+E94="$(printf "\e[94m")"      # foreground: bright blue
+E97="$(printf "\e[97m")"      # foreground: bright white
 CONF_PATH='/etc/mediawiki'
-REQUIRED_VARS=(
-  MARIADB_DATABASE
-  MARIADB_ROOT_PASSWORD
-  MARIADB_USER
-  MW_ADMIN_PASS
-  MW_ADMIN_USER
-  MW_DB_HOST
-  MW_DB_PORT
-  MW_SERVER_URL
-  MW_SITENAME
+REQUIRED_VARIABLES=(
+    MYSQL_DATABASE
+    MYSQL_ROOT_PASSWORD
+    MYSQL_USER
+    MW_ADMIN_PASS
+    MW_ADMIN_USER
+    MW_DB_HOST
+    MW_DB_PORT
+    MW_SERVER_URL
+    MW_SITENAME
 )
-WEB_ROOT='/var/lib/mediawiki'
 
 
 # Ensure all vars are set
-for var in "${REQUIRED_VARS[@]}"
+for _variable in "${REQUIRED_VARIABLES[@]}"
 do
-  if [[ -z "${var:-}" ]]
-  then
-    echo "ERROR: Required environment variable ${var} is not set"
-    exit 1
-  fi
+    if [[ -z "${!_variable:-}" ]]
+    then
+        _msg1="${E31}ERROR: Required environment variable is not set:"
+        _msg2=" ${_variable}${E0}"
+        echo "${_msg1}${_msg2}"
+        # Use exit code 0 to avoid triggering restart: on-failure
+        exit 0
+    fi
 done
-echo 'All required environment variables are present'
+echo "${E90}All required environment variables are present${E90}"
 
 
 echo "Waiting for database at ${MW_DB_HOST}:${MW_DB_PORT}..."
-while ! (echo > /dev/tcp/${MW_DB_HOST}/${MW_DB_PORT}) >/dev/null 2>&1
+while ! (echo > "/dev/tcp/${MW_DB_HOST}/${MW_DB_PORT}") >/dev/null 2>&1
 do
-  echo "Database not reachable yet. Retrying in 1s..."
-  sleep 1
+    echo "${E90}Database not reachable yet. Retrying in 1s...${E0}"
+    sleep 1
 done
 echo 'Database is up!'
 
@@ -44,27 +49,25 @@ echo 'Database is up!'
 # Install MediaWiki, if necessary
 if [[ ! -f "${CONF_PATH}/LocalSettings.php" ]]
 then
-  echo 'Beginning MediaWiki installation'
-  /usr/bin/php /usr/share/mediawiki/maintenance/install.php \
-    --confpath="${CONF_PATH}" \
-    --dbname="${MARIADB_DATABASE}" \
-    --dbpass="${MARIADB_ROOT_PASSWORD}" \
-    --dbserver="${MW_DB_HOST}:${MW_DB_PORT}" \
-    --dbuser="${MARIADB_USER}" \
-    --pass "${MW_ADMIN_PASS}" \
-    --scriptpath="" \
-    --server="${MW_SERVER_URL}" \
-    "$MW_SITENAME" "${MW_ADMIN_USER}"
-  #chmod 600 /var/www/wiki/LocalSettings.php
-  #chown www-data:www-data /var/www/wiki/LocalSettings.php
-  ln -s "${CONF_PATH}/LocalSettings.php" "${WEB_ROOT}/LocalSettings.php"
-  echo 'MediaWiki installation complete'
+    echo 'Beginning MediaWiki installation'
+    /usr/bin/php /usr/share/mediawiki/maintenance/run.php install \
+        --confpath="${CONF_PATH}" \
+        --dbname="${MYSQL_DATABASE}" \
+        --dbpass="${MYSQL_ROOT_PASSWORD}" \
+        --dbserver="${MW_DB_HOST}:${MW_DB_PORT}" \
+        --dbuser="${MYSQL_USER}" \
+        --pass "${MW_ADMIN_PASS}" \
+        --scriptpath="" \
+        --server="${MW_SERVER_URL}" \
+        "${MW_SITENAME}" "${MW_ADMIN_USER}"
+    # shellcheck disable=SC2016
+    echo '$wgArticlePath = "/wiki/$1";' >> "${CONF_PATH}/LocalSettings.php"
+    echo 'MediaWiki installation complete'
 else
-  echo 'Skipping MediaWiki installation (LocalSettings.php already exists)'
+    echo "${E90}Skipping MediaWiki installation (config present)${E0}"
 fi
 
 
 /sbin/apache2ctl -v
-# Start Apache in the foreground
-echo "${E1}Starting apache2 webserver: http://localhost:8080/${E0}"
+echo "${E97}Starting apache2 webserver: ${E94}${MW_SERVER_URL}/${E0}"
 /sbin/apache2ctl -D FOREGROUND -k start
