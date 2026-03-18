c6326874513c97a6d70f1e3b8758a4f2091013e7
Timid Robot Zehta <timid@creativecommons.org>
2026-03-18 17:19:38 +0100

    use one conditional startupservice.sh and add file extensions

--- config/startupservice.sh
+++ config/startupservice.sh
@@ -2,13 +2,30 @@
 set -o errexit
 set -o nounset
 
+CONF_PATH='/etc/mediawiki'
 # https://en.wikipedia.org/wiki/ANSI_escape_code
 E0="$(printf "\e[0m")"        # reset
 E31="$(printf "\e[31m")"      # foreground: red
 E90="$(printf "\e[90m")"      # foreground: bright black (gray)
 E94="$(printf "\e[94m")"      # foreground: bright blue
 E97="$(printf "\e[97m")"      # foreground: bright white
-CONF_PATH='/etc/mediawiki'
+FILE_EXTENSIONS=(
+    doc
+    docx
+    indd
+    m4v
+    odp
+    ods
+    odt
+    ogg
+    pdf
+    psd
+    svg
+    txt
+    xcf
+    xls
+    zip
+)
 REQUIRED_VARIABLES=(
     MARIADB_DATABASE
     MARIADB_ROOT_PASSWORD
@@ -37,6 +54,18 @@ done
 echo "${E90}All required environment variables are present${E90}"
 
 
+if [[ "${MW_SERVER_URL}" == 'http://localhost:8081' ]]
+then
+    CONTAINER='web-bullseye'
+    MW_INSTALL='/usr/bin/php /usr/share/mediawiki/maintenance/install.php'
+    APACHECTL='/usr/sbin/apache2ctl'
+else
+    CONTAINER='web'
+    MW_INSTALL='/usr/bin/php /usr/share/mediawiki/maintenance/run.php install'
+    APACHECTL='/sbin/apache2ctl'
+fi
+
+
 echo "Waiting for database at ${MW_DB_HOST}:${MW_DB_PORT}..."
 while ! (echo > "/dev/tcp/${MW_DB_HOST}/${MW_DB_PORT}") >/dev/null 2>&1
 do
@@ -50,7 +79,7 @@ echo 'Database is up!'
 if [[ ! -f "${CONF_PATH}/LocalSettings.php" ]]
 then
     echo 'Beginning MediaWiki installation'
-    /usr/bin/php /usr/share/mediawiki/maintenance/run.php install \
+    ${MW_INSTALL} \
         --confpath="${CONF_PATH}" \
         --dbname="${MARIADB_DATABASE}" \
         --dbpass="${MARIADB_ROOT_PASSWORD}" \
@@ -61,20 +90,36 @@ then
         --server="${MW_SERVER_URL}" \
         "${MW_SITENAME}" "${MW_ADMIN_USER}"
     # https://www.mediawiki.org/wiki/Manual:Short_URL/Apache
-    echo 'Enabling Short URL'
+    echo 'Enabling Short URL (wgArticlePath)'
     # shellcheck disable=SC2016
     echo '$wgArticlePath = "/wiki/$1";' >> "${CONF_PATH}/LocalSettings.php"
     # https://www.mediawiki.org/wiki/Manual:Configuring_file_uploads
-    echo 'Enabling Uploads'
+    echo 'Enabling Uploads (wgEnableUploads)'
     # shellcheck disable=SC2016
     sed -e's/\$wgEnableUploads = false;/$wgEnableUploads = true;/' -i \
         "${CONF_PATH}/LocalSettings.php"
+    # https://www.mediawiki.org/wiki/Manual:$wgFileExtensions
+    echo 'Adding file extensions (wgFileExtensions)'
+    for _ext in "${FILE_EXTENSIONS[@]}"
+    do
+        echo -n "  ${_ext}"
+        echo "\$wgFileExtensions[] = '${_ext}';" \
+            >> "${CONF_PATH}/LocalSettings.php"
+    done
+    echo
+
     echo 'MediaWiki installation complete'
 else
     echo "${E90}Skipping MediaWiki installation (config present)${E0}"
 fi
 
 
-/sbin/apache2ctl -v
-echo "${E97}Starting apache2 webserver: ${E94}${MW_SERVER_URL}/${E0}"
-/sbin/apache2ctl -D FOREGROUND -k start
+if [[ "${CONTAINER}" == 'web' ]]
+then
+    "${APACHECTL}" -v
+    echo "${E97}Starting apache2 webserver: ${E94}${MW_SERVER_URL}/${E0}"
+    "${APACHECTL}" -D FOREGROUND -k start
+else
+    echo "${E97}Sleeping 😴${E0}"
+    while true; do sleep 5 || break; done
+fi
